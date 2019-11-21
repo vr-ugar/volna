a0aa77282814c7f4fc01431452f187ac1dcf50c5
groznykh <groznykh.aleksandr@gmail.com>
2019-11-22 02:54:20 +0300

    mvp loigc

--- streamer.py
+++ streamer.py
@@ -5,6 +5,7 @@ import websockets
 
 import aiohttp
 from aiohttp import web
+from datetime import datetime
 
 
 logging.basicConfig()
@@ -14,10 +15,68 @@ USERS = set()
 
 ORDER = ['x', 'y', 'z', 'trigger']
 
+
+class Game:
+        
+        def __init__(self):
+                self.before = datetime.now()
+
+                self.FIELD = [0, 0, 0, 0, 0, 0, 0, 0]                                  
+                self.SPEED = (10**6 - 10000)  
+                self.DIRECTIONS = {"LEFT": -1, "RIGHT": 1}                                                                                                                           
+                self.BALL_POSITION = 0
+
+                self.DIRECTION = "RIGHT"
+                
+                self.trigger_state = 0
+
+        def main_loop(self, trigger_state):
+                # print(trigger_state, type(trigger_state))
+                if trigger_state is not None:
+                        trigger_state = int(trigger_state)
+                
+                now = datetime.now()
+                td = now - self.before
+                td = td.seconds * 10**6 + td.microseconds
+
+                if td > self.SPEED:
+                        try:
+                                self.before = now
+
+                                NEW_BALL_POSITION = self.BALL_POSITION + self.DIRECTIONS[self.DIRECTION]
+                                if NEW_BALL_POSITION == 0 or NEW_BALL_POSITION == 7:
+                                   if trigger_state == 1: 
+                                        if self.DIRECTION == "RIGHT":
+                                                self.DIRECTION = "LEFT"
+                                        else:
+                                                self.DIRECTION = "RIGHT"
+                                if NEW_BALL_POSITION == -1 or NEW_BALL_POSITION == 8:
+                                        print("game over")
+                                        raise 
+
+                                self.FIELD[NEW_BALL_POSITION] = 1
+                                self.FIELD[self.BALL_POSITION] = 0
+                                self.BALL_POSITION = NEW_BALL_POSITION
+                                print(self.FIELD)
+
+
+                        except IndexError:
+                                if self.DIRECTION == "RIGHT":
+                                        self.DIRECTION = "LEFT"
+                                else:
+                                        self.DIRECTION = "RIGHT"
+
+
+
+game = Game()
+
+
 async def notify_users():
     if USERS:       # asyncio.wait doesn't accept an empty list
         serialized_state = ';'.join(str(STATE.get(i, 0)) for i in ORDER)
-        print(serialized_state)
+        # print(serialized_state)
+        trigger_state = STATE.get(ORDER[-1], 0)
+        game.main_loop(trigger_state)
         await asyncio.wait([user.send_str(serialized_state) for user in USERS])
 
 
@@ -37,10 +96,6 @@ async def websocket_handler(request):
         USERS.remove(websocket)
 
     return websocket
-
-def game():
-    """return position of the ball"""
-    return
 
 
 async def hello(request):
